8d3ed9dceb4617f0ed4dcb00fef45d52826d0569
Gabriel Benavides <helmo.benavides@inacapmail.cl>
2026-06-17 12:54:50 -0400

    verificacion de que la app corren en localhost:5173, rendereizado de tarjetas y los datos se registran en consola de navegador

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import StickerCard from './components/StickerCard'
 import { stickers } from './data/stickers'
 
 function App() {
+  // Mostrar stickers en consola
+  console.log('Total de figuritas cargadas:', stickers.length)
+  console.log('Array de stickers:', stickers)
+
   return (
     <div className="container">
       <h1>Álbum Mundial 2026</h1>
